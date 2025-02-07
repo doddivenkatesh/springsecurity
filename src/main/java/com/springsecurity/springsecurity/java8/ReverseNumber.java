c85da5ed9c561471f0022295f0092fa7550cb4db
doddivenkatesh <doddivenkatesh145@gmail.com>
2025-02-07 16:28:12 +0530

    c

--- src/main/java/com/springsecurity/springsecurity/java8/ReverseNumber.java
+++ src/main/java/com/springsecurity/springsecurity/java8/ReverseNumber.java
@@ -2,13 +2,13 @@ package com.springsecurity.springsecurity.java8;
 
 public class ReverseNumber {
 
-	int number = 12345;
-	int revese =0;
+	static int number = 12345;
+	static int revese =0;
 	public static void main(String[] args) {
 		System.out.println("reverse number");
                while(number!=0){
 		       int remainder = number%10;
-		       reverse = reverse*10 + remainder;
+		       revese = revese*10 + remainder;
 		       number= number/10;
 	       }
 		
