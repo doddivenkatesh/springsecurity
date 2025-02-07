3b62167a8ce6d4310d65ad6e4f193e7151b1baec
doddivenkatesh <doddivenkatesh145@gmail.com>
2025-02-07 11:30:59 +0530

    MaxAndMin

--- src/main/java/com/springsecurity/springsecurity/java8/MaxAndMin.java
+++ src/main/java/com/springsecurity/springsecurity/java8/MaxAndMin.java
@@ -8,12 +8,12 @@ public class MaxAndMin {
 
 	public static void main(String[] args) {
 
-		 List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
-		int max= listOfIntegers.stream().max(Comparator.naturalOrder()).get();
-		   System.out.println("Maximum Element : "+max);
-		 int min =listOfIntegers.stream().min(Comparator.naturalOrder()).get();
-		 System.out.println("Minimum Element : "+min);
-		 
+		List<Integer> listOfIntegers = Arrays.asList(45, 12, 56, 15, 24, 75, 31, 89);
+		int max = listOfIntegers.stream().max(Comparator.naturalOrder()).get();
+		System.out.println("Maximum Element : " + max);
+		int min = listOfIntegers.stream().min(Comparator.naturalOrder()).get();
+		System.out.println("Minimum Element : " + min);
+
 	}
 
 }
