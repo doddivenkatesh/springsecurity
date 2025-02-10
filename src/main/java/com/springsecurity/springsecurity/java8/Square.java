17b04ba85550679d5a2254d82e7484c955bd8073
doddivenkatesh <doddivenkatesh145@gmail.com>
2025-02-10 18:20:08 +0530

    adding csv

---
+++ src/main/java/com/springsecurity/springsecurity/java8/Square.java
@@ -0,0 +1,10 @@
+package com.springsecurity.springsecurity.java8;
+
+public class Square {
+
+	public static void main(String[] args) {
+		int number =12;
+		double v=Math.sqrt(number);
+		System.out.println(v);
+	}
+}
